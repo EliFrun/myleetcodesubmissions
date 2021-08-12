7c05d817dfbc269370c3ac6d919813db27fe8bba
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2021-08-12 13:35:38 +0000

    Sync LeetCode submission - Merge Intervals (python3)

---
+++ problems/merge_intervals/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if len(intervals) == 1:
+            return intervals
+        intervals.sort(key=lambda x:x[0])
+        i = 1
+        current_interval = intervals[0]
+        ret = []
+        while i < len(intervals):
+            if intervals[i][0] <= current_interval[1]:
+                current_interval[1] = max(intervals[i][1], current_interval[1])
+            else:
+                ret.append(current_interval)
+                current_interval = intervals[i]
+                
+            i += 1
+        
+        ret.append(current_interval)
+        
+        return ret
