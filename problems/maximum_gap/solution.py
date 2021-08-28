8800912c0deb43378b20e5eefca3253de7593181
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2021-08-28 23:42:27 +0000

    Sync LeetCode submission - Maximum Gap (python3)

---
+++ problems/maximum_gap/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return 0
+        
+        nums.sort()
+        
+        m = 0
+        
+        for i in range(len(nums) - 1):
+            m = max(m, nums[i + 1] - nums[i])
+            
+            
+        return m
