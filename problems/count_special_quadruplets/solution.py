eabce1aa120b5182828c3f7abcbf84736ce072f3
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2021-10-06 17:35:58 +0000

    Sync LeetCode submission - Count Special Quadruplets (python3)

--- problems/count_special_quadruplets/solution.py
+++ problems/count_special_quadruplets/solution.py
@@ -2,11 +2,13 @@ class Solution:
     def countQuadruplets(self, nums: List[int]) -> int:
         ret = 0
         a = len(nums)
+        s = defaultdict(list)
+        for i, n in enumerate(nums):
+            s[n] += [i]
         for i in range(a - 3):
             for j in range(i + 1, a - 2):
                 for k in range(j + 1 , a - 1):
-                    for l in range(k + 1, a):
-                        if nums[i] + nums[j] + nums[k] == nums[l]:
-                            ret += 1
-                            
+                    n = nums[i] + nums[j] + nums[k] 
+                    if n in s:
+                        ret += len([x for x in s[n] if x > k])            
         return ret
