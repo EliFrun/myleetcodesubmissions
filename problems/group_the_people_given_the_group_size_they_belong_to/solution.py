baa98a0f6e2c6f2157bdd358316dbd1f6ba3645c
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2021-10-17 00:39:08 +0000

    Sync LeetCode submission - Group the People Given the Group Size They Belong To (python3)

--- problems/group_the_people_given_the_group_size_they_belong_to/solution.py
+++ problems/group_the_people_given_the_group_size_they_belong_to/solution.py
@@ -1,31 +1,16 @@
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
-        ret_list = []
-        prev_val = 0
-        current_val = 501
-        max_val = 0
-        for i in groupSizes:
-            if i < current_val and i > prev_val:
-                current_val = i
-            if i > max_val:
-                max_val = i
-
-        while current_val <= max_val:
-            temp_list = []
-            for i in range(len(groupSizes)):
-                if groupSizes[i] == current_val: 
-                    if len(temp_list) < current_val:
-                        temp_list.append(i)
-                    else:
-                        ret_list.append(temp_list)
-                        temp_list = [i]
-            
-            
-            ret_list.append(temp_list)                
-            prev_val = current_val
-            current_val = 501
-            for i in groupSizes:
-                if i < current_val and i > prev_val:
-                    current_val = i
-            
-        return ret_list
+        d = defaultdict(lambda: [[]])
+        
+        for i,v in enumerate(groupSizes):
+            if len(d[v][-1]) < v:
+                d[v][-1].append(i)
+            else:
+                d[v].append([i])
+                
+        ret = []
+        for _,v in d.items():
+            ret += v
+                
+        return ret
+        
