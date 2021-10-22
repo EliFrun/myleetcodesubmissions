d488fd3e0875adfc80d3c6482cb781a54a42b7af
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2021-10-22 02:18:16 +0000

    Sync LeetCode submission - Find Nearest Point That Has the Same X or Y Coordinate (python3)

--- problems/find_nearest_point_that_has_the_same_x_or_y_coordinate/solution.py
+++ problems/find_nearest_point_that_has_the_same_x_or_y_coordinate/solution.py
@@ -1,17 +1,20 @@
 class Solution:
     def nearestValidPoint(self, x: int, y: int, points: List[List[int]]) -> int:
-        dis = 100000
         ret = -1
-        for index, lis in enumerate(points):
-            i, j = lis[0], lis[1]
-            if i != x and j != y:
-                continue
-            else:
-                a = abs(i - x) + abs(j - y)
-                if  a < dis:
-                    dis = a
-                    ret = index
+        closest_distance = 1_000_000
+        for i, (a, b) in enumerate(points):
+            if a == x:
+                dis = abs(y - b)
+                if dis < closest_distance:
+                    closest_distance = dis
+                    ret = i
+            elif b == y:
+                dis = abs(x - a)
+                if dis < closest_distance:
+                    closest_distance = dis
+                    ret = i
                     
         return ret
+        
                 
         
