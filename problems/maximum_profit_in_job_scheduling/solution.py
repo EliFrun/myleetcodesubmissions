39337feaad8a400f2b0d8e9fe5c18dbad8a96db8
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2021-10-22 02:10:08 +0000

    Sync LeetCode submission - Maximum Profit in Job Scheduling (python3)

--- problems/maximum_profit_in_job_scheduling/solution.py
+++ problems/maximum_profit_in_job_scheduling/solution.py
@@ -1,34 +1,28 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        schedules = sorted(zip(startTime, endTime, profit), key=lambda x: (x[0], x[1]))
-        cache = [-1] * len(endTime)
-        # list is sorted so this could be O(logn)
-        def indexAfterEnd(end, idx):
-            nonlocal schedules
-            i, j = idx, len(schedules)
-            while i + 1 < j:
-                mid = (i+j)//2
-                if schedules[mid][0] >= end:
-                    j = mid
-                else:
-                    i = mid
-            
-            if min(i, j) >= len(schedules):
-                return min(i, j)
-            return i if schedules[i][0] >= end else j
-            
+        jobs = sorted(zip(startTime, endTime, profit), key=lambda x: x[0])
+        cache = [-1] * len(startTime)
+        
+        def findBestAfterEnd(end, idx):
+            nonlocal jobs
+            for i in range(idx, len(jobs)):
+                if jobs[i][0] >= end:
+                    return i
+                
+            return len(jobs)
         
         def findOptimal(idx):
-            nonlocal schedules
-            if idx >= len(schedules):
-                return 0
-
+            nonlocal jobs
             nonlocal cache
-            if cache[idx] >= 0:
+            if idx >= len(jobs):
+                return 0
+            if cache[idx] > 0:
                 return cache[idx]
             
-            with_curr = schedules[idx][2] + findOptimal(indexAfterEnd(schedules[idx][1], idx))
-            cache[idx] = max(with_curr, findOptimal(idx + 1))
+            including_curr = jobs[idx][2] + findOptimal(findBestAfterEnd(jobs[idx][1], idx))
+            without_curr = findOptimal(idx + 1)
+            
+            cache[idx] = max(including_curr, without_curr)
             return cache[idx]
         
         return findOptimal(0)
