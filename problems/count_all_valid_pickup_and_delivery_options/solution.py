d51277d276aa399a50c833f77b48711f5095ab61
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2021-10-26 01:26:42 +0000

    Sync LeetCode submission - Count All Valid Pickup and Delivery Options (python3)

--- problems/count_all_valid_pickup_and_delivery_options/solution.py
+++ problems/count_all_valid_pickup_and_delivery_options/solution.py
@@ -2,5 +2,6 @@ class Solution:
     def countOrders(self, n: int) -> int:
         if n == 1:
             return 1
-        return ((n) * (2 * n - 1) * self.countOrders(n - 1)) % 1_000_000_007
+        mod = 1_000_000_007
+        return ((n * (2 * n - 1)) % mod) * (self.countOrders(n - 1) % mod) % mod
         
