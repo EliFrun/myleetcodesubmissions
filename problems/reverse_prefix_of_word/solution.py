077e34af266c7276985c51e0ab08d5a49afd1afb
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2022-09-10 20:09:50 +0000

    Sync LeetCode submission - Reverse Prefix of Word (python3)

--- problems/reverse_prefix_of_word/solution.py
+++ problems/reverse_prefix_of_word/solution.py
@@ -1,5 +1,7 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
-        a = word.find(ch)
-        a = a if a > 0 else 0
-        return word[a::-1] + word[a + 1:]
+        if ch in word:
+            return ''.join(list(word)[word.find(ch)::-1]) + ''.join(list(word)[word.find(ch) + 1:])
+        else:
+            return word
+        
